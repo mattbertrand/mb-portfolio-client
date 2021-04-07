22b95cd7b72c4cab8d973381b448de95467de0c1
github username <github email address>
2021-04-07 09:23:10 -0700

    finalized wiring redux bu createing a store and adding Provider to the DOM

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import postsReducer from './reducers/postsReducer'
+
+const store = createStore(postsReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
