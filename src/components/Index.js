ccbd34f0ad4c68386f6a8aac9649a9d20fb435e4
github username <github email address>
2021-04-07 16:13:19 -0700

    updated Post

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Index extends Component {
     render() {
+
+        
         return (
             <div>
                 List Page
@@ -10,4 +13,10 @@ class Index extends Component {
     }
 }
 
-export default Index
+const mapStateToProps = state => {
+    return {
+        posts: state.posts
+    }
+}
+
+export default connect(mapStateToProps)(Index)
--- src/components/Post.js
+++ src/components/Post.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react'
 
 class Post extends Component {
     render() {
+        const { title, content, category, image_link} = this.props
         return (
             <div>
-                Post
+                <h3>{ title }</h3>
+                <p>{ content }</p>
+                <p>{ category }</p>
+                <p>{ image_link }</p>
             </div>
         )
     }
