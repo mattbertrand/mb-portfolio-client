b513d4549c71962bebfac45f1f8ab72def692600
github username <github email address>
2021-04-07 16:54:27 -0700

    created a form to add new project

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
 
 class NewPost extends Component {
+    state = {
+        title: '',
+        content: '',
+        category: '',
+        image_link: ''
+    }
     render() {
         return (
             <div>
-                New Post Page
+                <h3>Create New Project</h3>
+                <form>
+                    <div>
+                        <label htmlFor="title">Title</label>
+                        <input type="text" name="title" id="title" value={this.state.title} onChange={ this.handleChange }/>
+                    </div>
+                    <div>
+                        <label htmlFor="content">Content</label>
+                        <textarea type="text" name="content" id="content" value={this.state.content} onChange={ this.handleChange }/>
+                    </div>
+                    <div>
+                        <label htmlFor="category">Category</label>
+                        <input type="text" name="category" id="category" value={this.state.category} onChange={ this.handleChange }/>
+                    </div>
+                    <div>
+                        <label htmlFor="image_link">Image</label>
+                        <input type="text" name="image_link" id="image_link" value={this.state.image_link} onChange={ this.handleChange }/>
+                    </div>
+                </form>
             </div>
         )
     }
