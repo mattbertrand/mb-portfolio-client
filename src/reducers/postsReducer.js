c2cf9144d89fd3e416acc6182d0e2d527407ed2b
github username <github email address>
2021-04-07 17:09:47 -0700

    added a case ADD_POST to reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -16,6 +16,11 @@ const postsReducer = (state=initialState, action) => {
                 loading: false,
                 posts: action.posts
             }
+        case "ADD_POST":
+            return {
+                ...state,
+                posts: [...state.posts, action.post]
+            }
         default:
             return state
     }
